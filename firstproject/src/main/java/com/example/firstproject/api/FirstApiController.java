2f98c33ba06040d01dcff1e651cacfa5f5e7c7dc
Kiwon <mrkiwonseo@gmail.com>
2024-06-27 12:31:57 +0900

    REST API 구현하기 GET, POST, PATCH, DELETE 구현 Article에 patch메서드 추가

---
+++ firstproject/src/main/java/com/example/firstproject/api/FirstApiController.java
@@ -0,0 +1,19 @@
+package com.example.firstproject.api;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+public class FirstApiController {
+    @GetMapping("/api/hello")
+    public String hello(){
+        return "hello world!";
+    }
+
+
+}
